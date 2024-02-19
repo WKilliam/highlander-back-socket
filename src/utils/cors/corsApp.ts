9feef42661d733f4684a4adac21f400ef4bda0d0
BearmanAdmin <william.k97430@gmail.com>
2024-02-19 16:01:23 +0100

    fix cros

--- src/utils/cors/corsApp.ts
+++ src/utils/cors/corsApp.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export const corsApp = {
-    origin: "http://localhost:4200",
+    origin: "http://195.154.114.37:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type, Authorization"],
     credentials: true
